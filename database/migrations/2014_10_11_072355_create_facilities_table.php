87138949d59f56ffe4a14284011b7aa48875d21d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-13 13:00:35 +0530

    Created project and added basic migrations

---
+++ database/migrations/2014_10_11_072355_create_facilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('facility_code');
+            $table->string('facility_name');
+            $table->foreignId('facility_type_id')->constrained('facility_types', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+}
