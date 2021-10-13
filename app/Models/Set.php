ac6e89b14108f648ea00b3015afe3267c914d367
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-13 13:33:41 +0530

    Added properties for Set model

--- app/Models/Set.php
+++ app/Models/Set.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Set extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'created_by',
+        'updated_by',
+        'facility_id',
+    ];
+
+    public function creator(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function updator(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function facility(){
+        return $this->belongsTo(Facility::class);
+    }
 }
