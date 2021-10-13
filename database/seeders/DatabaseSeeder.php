b8a79fb0127856ef1a480f7ef33e2d98903a38dc
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-13 15:05:29 +0530

    Added seeders to DataBaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        $this->call([
+            ArticleTransactionTypeSeeder::class,
+            ArticleTypeSeeder::class,
+            BagTransactionTypeSeeder::class,
+            BagTypeSeeder::class,
+            FacilityTypeSeeder::class,
+            FacilitySeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
