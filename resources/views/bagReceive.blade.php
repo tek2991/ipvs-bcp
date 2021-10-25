f64c6e53eddfab4a54acc8a75dfc1532eff1d9f3
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-25 10:14:17 +0530

    reduced spacing

--- resources/views/bagReceive.blade.php
+++ resources/views/bagReceive.blade.php
@@ -11,7 +11,7 @@
     </x-slot>
 
     @if (session('success'))
-        <div class="pt-6">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-green-300 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
@@ -23,7 +23,7 @@
     @endif
 
     @if (session('error'))
-        <div class="pt-6">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-red-300 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
@@ -35,7 +35,7 @@
     @endif
 
     @if ($errors->any())
-        <div class="py-12">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
@@ -46,7 +46,7 @@
         </div>
     @endif
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <form action="{{ route('bag-receive.store') }}" method="post">
@@ -100,7 +100,7 @@
     </div>
 
     @if ($bags_received->total() > 0)
-        <div class="pt-6">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="p-6">
                     {{ $bags_received->links() }}
@@ -108,18 +108,18 @@
             </div>
         </div>
 
-        <div class="pt-6 pb-6">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="text-lg font-semibold p-4">
                     Received bags
                 </div>
             </div>
             @foreach ($bags_received as $bag_received)
-                <div class="p-2">
+                <div class="p-1">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="p-4 bg-white border-b border-gray-200">
                             <div class="grid grid-cols-5 gap-1">
-                                <div class="text-lg font-semibold mb-3">{{ $bag_received->bag_no }}</div>
+                                <div class="text-lg font-semibold">{{ $bag_received->bag_no }}</div>
                                 <span>{{ $bag_received->bagType->description }}({{ $bag_received->bagTransactionType->description }})</span>
                                 <span>{{ $bag_received->fromFacility->name }}</span>
                                 <span>{{ $bag_received->creator->name }}</span>
@@ -131,7 +131,7 @@
             @endforeach
         </div>
 
-        <div class="pt-6">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="p-6">
                     {{ $bags_received->links() }}
@@ -139,7 +139,7 @@
             </div>
         </div>
     @else
-        <div class="py-12">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-gray-400 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 font-semibold">
