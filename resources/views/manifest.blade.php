4eb243fb3c41ceaf6178fc6519569aa9cdb35c48
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-25 15:34:32 +0530

    UI fix

--- resources/views/manifest.blade.php
+++ resources/views/manifest.blade.php
@@ -93,7 +93,7 @@
                 <div class="p-1">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="p-4 bg-white border-b border-gray-200">
-                            <div class="grid grid-cols-5 gap-1">
+                            <div class="flex justify-between">
                                 <div class="text-lg font-semibold">{{ $bag->bag_no }}</div>
                                 <span>{{ $bag->bagType->description }}({{ $bag->bagTransactionType->description }})</span>
                                 <span>{{ $bag->fromFacility->name }} -> {{ $bag->toFacility->name }}</span>
