6688deeb44a64b27355dde589ba02abc10812563
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-10-25 15:36:26 +0530

    UI fix

--- resources/views/pendingArticles.blade.php
+++ resources/views/pendingArticles.blade.php
@@ -29,10 +29,10 @@
                 <div class="p-1">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="p-4 bg-white border-b border-gray-200">
-                            <div class="grid grid-cols-5 gap-1">
+                            <div class="flex justify-between">
                                 <div class="text-lg font-semibold">{{ $article->article_no }} ({{ $article->articleType->name }})</div>
                                 <span>{{ $article->articleTransactionType->description }}</span>
-                                <span>{{ $article->fromFacility->name }} -> {{ $article->toFacility->name }}</span>
+                                <span>{{ $article->fromFacility->name }} -> {{ $article->toFacility->name }} ({{ $article->bag->bag_no }})</span>
                                 <span>{{ $article->updated_at }}</span>
                                 <span>{{ $article->creator->name }}</span>
                             </div>
