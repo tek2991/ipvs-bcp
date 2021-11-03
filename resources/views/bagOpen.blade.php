5a5cdebaa552304d258ffad17b3256e20f4db225
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-11-03 12:04:03 +0530

    Added timeout for scrolTo function

--- resources/views/bagOpen.blade.php
+++ resources/views/bagOpen.blade.php
@@ -50,7 +50,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <form action="{{ route('bag-open.bagScan') }}" method="get">
-                    @csrf
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
                             <div class="grid grid-cols-2 gap-16">
--- resources/views/bagOpenArticleScan.blade.php
+++ resources/views/bagOpenArticleScan.blade.php
@@ -17,7 +17,9 @@
                 history.scrollRestoration = 'manual';
             }
             // This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
-            window.scrollTo(0, 0);
+            setTimeout(function() {
+                window.scrollTo(0, 0);
+            }, 50);
         </script>
     @endif
 
