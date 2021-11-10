07432e3a153b3e1eb5f50d3237a9926615791b00
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-11-10 11:49:47 +0530

    Added Filters

--- app/Http/Controllers/FacilityMappingController.php
+++ app/Http/Controllers/FacilityMappingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Facility;
+use App\Models\FacilityType;
+use App\Models\ReportingCircle;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,11 +28,21 @@ class FacilityMappingController extends Controller
         }
         $this->validate($request, [
             'facility_id' => 'bail|required|exists:facilities,id',
+            'reporting_circle_id' => 'nullable|integer|exists:reporting_circles,id',
+            'facility_type_id' => 'nullable|integer|exists:facility_types,id',
         ]);
+
+        $reporting_circles = ReportingCircle::get();
+        $facility_types = FacilityType::get();
+
+        $reporting_circle_id = $request->reporting_circle_id ?? '%%';
+        $facility_type_id = $request->facility_type_id ?? '%%';
+
+        $active_facilities = Facility::where('is_active', true)->orderBy('name')->get();
         $selected_facility = Facility::find($request->facility_id);
         $facility_ids = $selected_facility->mappedFacilities()->get()->modelKeys();
-        $facilities = Facility::whereIn('id', $facility_ids)->orderBy('pincode')->with('district', 'reportingCircle')->paginate();
+        $facilities = Facility::whereIn('id', $facility_ids)->where('reporting_circle_id', 'like', $reporting_circle_id)->where('facility_type_id', 'like', $facility_type_id)->orderBy('pincode')->with('district', 'reportingCircle')->paginate();
 
-        return view('facilityMappingShow', compact('facilities'));
+        return view('facilityMappingShow', compact('facilities', 'reporting_circles', 'facility_types', 'active_facilities', 'request'));
     }
 }
--- resources/views/facilityMappingShow.blade.php
+++ resources/views/facilityMappingShow.blade.php
@@ -60,6 +60,72 @@
     @endif
 
 
+    @if ($errors->any())
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <form action="{{ route('facility-mapping.show') }}" method="get">
+                    <div class="p-6 flex items-center justify-between">
+                        <div>
+                            <label for="facility_id" class="pt-2 text-lg font-semibold">Map for Facility: </label>
+                            <select name="facility_id" id="facility_id"
+                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                @foreach ($active_facilities as $facility)
+                                    <option value="{{ $facility->id }}"
+                                        {{ $request->facility_id == $facility->id ? 'selected' : '' }}
+                                        >
+                                        {{ $facility->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <label for="facility_type_id" class="pt-2 text-lg font-semibold">Facility Type: </label>
+                            <select name="facility_type_id" id="facility_type_id"
+                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                @foreach ($facility_types as $facility_type)
+                                    <option value="{{ $facility_type->id }}"
+                                        {{ $request->facility_type_id == $facility_type->id ? 'selected' : '' }}
+                                        >
+                                        {{ $facility_type->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <label for="reporting_circle_id" class="pt-2 text-lg font-semibold">Reporting Circle: </label>
+                            <select name="reporting_circle_id" id="reporting_circle_id"
+                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                @foreach ($reporting_circles as $reporting_circle)
+                                    <option value="{{ $reporting_circle->id }}"
+                                        {{ $request->reporting_circle_id == $reporting_circle->id ? 'selected' : '' }}
+                                        >
+                                        {{ $reporting_circle->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <button type="submit"
+                            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     @if ($facilities->total() > 0)
         <div class="py-2">
